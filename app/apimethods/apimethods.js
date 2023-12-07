aa8f578e9bddb9aa8307682201c0f17c10e6a483
Gaizka <gaizkamg@gmail.com>
2023-12-07 21:42:08 +0100

    https://todo-list-api-s0bz.onrender.com

--- app/apimethods/apimethods.js
+++ app/apimethods/apimethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = 'https://todo-list-api-s0bz.onrender.com';
 
 export const getTasks = async () => {
     try {
