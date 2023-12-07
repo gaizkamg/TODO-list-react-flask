11d161d963d1561d7f42681c95df1cabb86a98a9
Gaizka <gaizkamg@gmail.com>
2023-12-07 12:20:14 +0100

    fix component name

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import TaskList from '/app/components/taskList';
+import TaskList from './components/TaskList';
 
 
 export default function Home() {
