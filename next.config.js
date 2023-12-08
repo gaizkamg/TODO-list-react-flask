a364e7802c363e6688202d6f25af3a006dae7bdc
Gaizka <gaizkamg@gmail.com>
2023-12-08 20:29:54 +0100

    google font cors

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "https://todo-list-api-s0bz.onrender.com" }, 
                     { key: "Access-Control-Allow-Origin", value: "http://localhost:5000" }, 
+                    { key: "Access-Control-Allow-Origin", value: "https://fonts.googleapis.com/" }, 
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
